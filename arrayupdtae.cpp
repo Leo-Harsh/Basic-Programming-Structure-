e67488712f48ee850e6888f4dbe0047122fa463d
Leo-Harsh <41663030+Leo-Harsh@users.noreply.github.com>
2020-07-18 06:26:33 +0530

    Update arrayupdtae.cpp

--- arrayupdtae.cpp
+++ arrayupdtae.cpp
@@ -1,3 +1,6 @@
+//Program for add a node in array at any position 
+//(Level Easy)
+//PS:: Run program first for better understanding.
 #include<iostream>
 using namespace std;
 int main()
@@ -7,20 +10,20 @@ int main()
     cin>>a;
     int ar[a];
     cout<<"enter"<<a<<"value";
-    for(i=0;i<a;i++)
+    for(i=0;i<a;i++)    //Taking Input from user to create an array;
     {
         cin>>ar[i];
     }
-    cout<<"before Update";
+    cout<<"before Update";    //Printing Value before update
     for(int b=0;b<a;b++)
     {
         cout<<"LA["<<b<<"]"<<"="<<ar[b]<<endl;
     }
-    cout<<"value which youu want to update and at what place";
-    cin>>k>>c;
-    ar[c]=k;
+    cout<<"value which youu want to update and at what place"; 
+    cin>>k>>c;                                 //Input value and node
+    ar[c]=k;                                   //Update
     cout<<"After Update";
-    for(int b=0;b<a;b++)
+    for(int b=0;b<a;b++)             //Printing Value After Update;
     {
         cout<<"LA["<<b<<"]"<<"="<<ar[b]<<endl;
     }
