4c47e93fd0589dc7d99fade29d6b14e5d357ccfb
Leo-Harsh <41663030+Leo-Harsh@users.noreply.github.com>
2020-07-18 06:22:58 +0530

    Update arraysearch.cpp

--- arraysearch.cpp
+++ arraysearch.cpp
@@ -1,24 +1,28 @@
+//Searching a number in array by using simple linear search method;
+//Level Easy
+//PS run code for better understanding.
 #include<iostream>
 #include<stddef.h>
 using namespace std;
 int main()
 {
     int i,a,b,c,k;
-   int ar[10]={1,2,3,4,5,6,7,8,9,0};
+   int ar[10]={1,2,3,4,5,6,7,8,9,0};    //Initializing array 
    cout<<"value which you want to search";
-   cin>>a;
+   cin>>a;                                //Input value which user want to search
    k=0;
-   while(k<10)
+   while(k<10)            //Traversing the loop .
    {
-    if (ar[k]==a)
+    if (ar[k]==a)                    // Condition for getting the value 
     {
         c=k+1;
+        cout<<"Value found in NOde"<<c<<endl; //if value found
         }
 
         k=k+1;
    }
-   if(c==NULL)
-   {
+   if(c==NULL)                 //If value not found
+   { 
        cout<<"Value not found SORRY";
    }
    }
