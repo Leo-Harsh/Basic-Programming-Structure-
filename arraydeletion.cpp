30c521ec0e8ae36bd9b6ce0552fd8ef559a8cff0
Leo-Harsh <41663030+Leo-Harsh@users.noreply.github.com>
2020-07-18 06:16:50 +0530

    Update arraydeletion.cpp

--- arraydeletion.cpp
+++ arraydeletion.cpp
@@ -1,18 +1,21 @@
+//Program for deleting the array node at any position by taking input from user.
+//level Easy
+//PS:: Run the code first for better understanding.
 #include<iostream>
 using namespace std;
 int main()
 {
     int i,a,b,c,k;
-    cout<< "enter the size of array";
+    cout<< "enter the size of array"; 
     cin>>a;
     int ar[a];
     cout<<"enter"<<a<<"value";
-    for(i=0;i<a;i++)
+    for(i=0;i<a;i++)            //Creating a array.
     {
         cin>>ar[i];
     }
     cout<<"before Update";
-    for(int b=0;b<a;b++)
+    for(int b=0;b<a;b++)        //Printing a Array
     {
         cout<<"LA["<<b<<"]"<<"="<<ar[b]<<endl;
     }
@@ -20,14 +23,14 @@ int main()
     cout<<"enter the position which you want to delete the array";
     cin>>k;
 
-    a=a-1;
+    a=a-1;                  // Subtract the value by one because we are deleting one node.
 
-    for(k;a>k;k++)
+    for(k;a>k;k++)          //traversing the array
     {
-        ar[k]=ar[k+1];
+        ar[k]=ar[k+1];        //shifting the node 
 
     }
-     cout<<"after Update";
+     cout<<"after Update";   //printing the array after deletion
     for(int b=0;b<a;b++)
     {
         cout<<"LA["<<b<<"]"<<"="<<ar[b]<<endl;
